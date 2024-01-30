4f69c181f7bd72472f18b1600f8e30531819bd1c
Maxim Shevcov <makc.shewtsov@gmail.com>
2024-01-30 13:25:03 +0400

    fix bag header

--- scripts/script.js
+++ scripts/script.js
@@ -13,20 +13,26 @@ window.addEventListener('scroll', function () {
 let menu = document.getElementById('open');
 let close = document.getElementById("close");
 
-document.getElementById('burger').onclick = function burger() {
+document.getElementById('burger').onclick = function () {
     menu.classList.add('open');
     close.classList.add('open');
     document.body.classList.add('body');
 }
 
-document.getElementById('close').onclick = (function closes() {
+document.getElementById('close').onclick = (function () {
     menu.classList.remove('open');
     close.classList.remove('open');
     document.body.classList.remove('body');
 });
 
+document.getElementById('bag').onclick = (function () {
+    menu.classList.remove('open');
+    close.classList.remove('open');
+    document.body.classList.remove('body');
+})
+
 document.addEventListener('keydown', function (event) {
-    const key = event.key; // const {key} = event; in ES6+
+    const key = event.key;
     if (key === "Escape") {
         menu.classList.remove('open');
         close.classList.remove('open');
